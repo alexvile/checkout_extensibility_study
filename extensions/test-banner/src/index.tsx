a2d06bee82a4bc8650092b1f9067f44f9d6abe26
Alexei Solotvinskiy <alexei20293@yahoo.com>
2023-04-13 10:56:44 +0300

    add(settings): base settings

--- extensions/test-banner/src/index.tsx
+++ extensions/test-banner/src/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React from "react";
 import {
-  useExtensionApi,
   render,
   Banner,
-  useTranslate,
-} from '@shopify/checkout-ui-extensions-react';
+  useSettings,
+  TextBlock,
+  useTotalAmount,
+} from "@shopify/checkout-ui-extensions-react";
 
-render('Checkout::Dynamic::Render', () => <App />);
+render("Checkout::Dynamic::Render", () => <App />);
+
+interface ISettings {
+  [key: string]: any;
+  show_notification: boolean;
+  sum_level_1: number;
+  msg_level_1: string;
+}
 
 function App() {
-  const {extensionPoint} = useExtensionApi();
-  const translate = useTranslate();
+  const { show_notification, sum_level_1, msg_level_1 } =
+    useSettings<ISettings>();
+  const { amount, currencyCode } = useTotalAmount();
+
+  const msg1 = msg_level_1.replace(
+    "[[difference]]",
+    (sum_level_1 - amount).toFixed(2)
+  );
+
   return (
-    <Banner title="test-banner">
-      {translate('welcome', {extensionPoint})}
+    <Banner>
+      {show_notification && amount < sum_level_1 && (
+        <TextBlock>
+          Notification:
+          {msg1}
+        </TextBlock>
+      )}
+
+      <TextBlock>Info</TextBlock>
+      <TextBlock>Amount for 1 level : {sum_level_1} </TextBlock>
+      <TextBlock>
+        {amount}
+        {currencyCode}
+      </TextBlock>
     </Banner>
   );
 }
+// {show_notification && amount < sum_level_1 && (
+//   <TextBlock>
+//     Notification:
+//     {/* {msg1} */}
+//   </TextBlock>
+// )}
