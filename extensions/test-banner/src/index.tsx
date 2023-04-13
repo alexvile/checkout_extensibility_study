0e7bd6b24d0e6418c4c8eab31cf5317afbc83afc
Alexei Solotvinskiy <alexei20293@yahoo.com>
2023-04-13 14:49:38 +0300

    add(feature): 2 step discount messages

--- extensions/test-banner/src/index.tsx
+++ extensions/test-banner/src/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import {
   render,
   Banner,
   useSettings,
   TextBlock,
   useTotalAmount,
+  useDiscountAllocations,
+  View,
 } from "@shopify/checkout-ui-extensions-react";
 
 render("Checkout::Dynamic::Render", () => <App />);
@@ -14,39 +15,66 @@ interface ISettings {
   show_notification: boolean;
   sum_level_1: number;
   msg_level_1: string;
+  sum_level_2: number;
+  msg_level_2: string;
 }
 
 function App() {
-  const { show_notification, sum_level_1, msg_level_1 } =
-    useSettings<ISettings>();
-  const { amount, currencyCode } = useTotalAmount();
+  const {
+    show_notification,
+    sum_level_1,
+    msg_level_1,
+    sum_level_2,
+    msg_level_2,
+  } = useSettings<ISettings>();
 
-  const msg1 = msg_level_1.replace(
-    "[[difference]]",
-    (sum_level_1 - amount).toFixed(2)
-  );
+  const { amount, currencyCode } = useTotalAmount();
+  const [appliedDiscount] = useDiscountAllocations();
+
+  let appliedDiscountValue = 0;
+  let originalPrice = amount;
+
+  if (appliedDiscount) {
+    appliedDiscountValue = appliedDiscount.discountedAmount?.amount;
+    originalPrice = amount + appliedDiscountValue;
+  }
+
+  const buildMessage = (baseMsg: string, sumLevel: number) => {
+    return baseMsg?.replace(
+      "[[difference]]",
+      (sumLevel - originalPrice).toFixed(2)
+    );
+  };
+
+  const msg1 = buildMessage(msg_level_1, sum_level_1);
+  const msg2 = buildMessage(msg_level_2, sum_level_2);
 
   return (
     <Banner>
-      {show_notification && amount < sum_level_1 && (
-        <TextBlock>
-          Notification:
-          {msg1}
-        </TextBlock>
+      {show_notification && (
+        <View>
+          {originalPrice < sum_level_1 && (
+            <TextBlock>
+              Notification:
+              {msg1}
+            </TextBlock>
+          )}
+          {sum_level_1 < originalPrice && originalPrice < sum_level_2 && (
+            <TextBlock>
+              Notification:
+              {msg2}
+            </TextBlock>
+          )}
+        </View>
       )}
 
-      <TextBlock>Info</TextBlock>
+      <TextBlock>All Info</TextBlock>
       <TextBlock>Amount for 1 level : {sum_level_1} </TextBlock>
+      <TextBlock>Amount for 2 level : {sum_level_2} </TextBlock>
       <TextBlock>
-        {amount}
-        {currencyCode}
+        Original: {originalPrice} {currencyCode}
       </TextBlock>
+      <TextBlock>Final: {amount}</TextBlock>
     </Banner>
   );
 }
-// {show_notification && amount < sum_level_1 && (
-//   <TextBlock>
-//     Notification:
-//     {/* {msg1} */}
-//   </TextBlock>
-// )}
